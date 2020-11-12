b71d9311e0f70381103f21364a8c69684a0fb16b
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2020-11-12 08:01:05 -0600

    Ya se controlan 2 servos
    Es como el código del lab en el que se controlaban 2

--- Proyecto3.X/Principal.asm
+++ Proyecto3.X/Principal.asm
@@ -1,14 +1,166 @@
-; TODO INSERT CONFIG CODE HERE USING CONFIG BITS GENERATOR
+ ;*******************************************************************************
+;
+;   Filename:	    Proyecto3 -> Principal.asm
+;   Date:		    12/11/2020
+;   File Version:	    v.1
+;   Author:		    Noel Prado
+;   Company:	    UVG
+;   Description:	    Proyecto3
+;
+;*******************************************************************************  
 
-RES_VECT  CODE    0x0000            ; processor reset vector
+    #include "p16f887.inc"
+
+; CONFIG1
+; __config 0xE0D5
+    __CONFIG _CONFIG1, _FOSC_INTRC_NOCLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; CONFIG2
+; __config 0xFFFF
+    __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
+    
+    GPR_VAR	    UDATA
+    STATUS_TEMP	RES 1
+    W_TEMP	RES 1
+    
+    
+    
+    RES_VECT  CODE    0x0000            ; processor reset vector
     GOTO    START                   ; go to beginning of program
 
-; TODO ADD INTERRUPTS HERE IF USED
+
+ISR_VECT    CODE 0X0004
+ 
+    
+PUSH:
+    MOVWF	    W_TEMP
+    SWAPF	   STATUS, W
+    MOVWF	   STATUS_TEMP 
+        
+INT_ADC:
+    BCF	PIR1, ADIF
+    BANKSEL ADRESH	     
+    MOVF ADRESH,W		;GUARDAR 8 BITS EN RESULTHI
+    BTFSC   ADCON0, 2
+    GOTO	 CAMBIO_CANAL
+    MOVWF CCPR1L
+
+    BANKSEL ADCON0
+    BSF   ADCON0, 2
+    GOTO REINICIOADC
+    
+CAMBIO_CANAL:
+    ;SI YA CAMBIO EL CANAL EJECUTA ESTO
+    MOVWF   CCPR2L
+    BANKSEL ADCON0  
+    BCF   ADCON0, 2
+    GOTO    REINICIOADC
+
+INT_TMR2:
+    BCF	PIR1, TMR2IF
+    GOTO POP
+    
+REINICIOADC:
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    BANKSEL ADCON0
+    BSF ADCON0, GO
+    
+    
+POP:
+    SWAPF	    STATUS_TEMP, W
+    MOVWF	    STATUS
+    SWAPF	    W_TEMP, F
+    SWAPF	    W_TEMP, W
+    BSF	    INTCON, GIE
+ 
+    RETFIE
 
 MAIN_PROG CODE                      ; let linker place main program
 
 START
 
+    CALL    CONFIG_IO
+    
+    LOOP:
+ 
+    
+    GOTO LOOP
     GOTO $                          ; loop forever
 
+    
+    
+    
+    CONFIG_IO
+    
+    BANKSEL PORTA
+    CLRF    PORTC
+    CLRF    PORTA
+    CLRF    PORTB
+    
+    BANKSEL TRISA
+    CLRF    TRISB
+    CLRF    TRISC
+    BSF	TRISA, 0
+    BSF	TRISA, 1
+    BANKSEL ADCON1
+    MOVLW B'00000000'
+    MOVWF   ADCON1
+    
+    BANKSEL ANSEL
+    BSF	ANSEL, 0
+    BSF	ANSEL, 1
+    BANKSEL ADCON0
+    MOVLW   B'01000001'	
+    MOVWF   ADCON0
+    
+    ;PARTE DE LA INTERRUPCION 
+    BANKSEL TRISA
+    BSF	PIE1, ADIE
+    BSF	PIE1, TMR2IE
+    BANKSEL PORTA
+    BSF	INTCON, GIE
+    BSF	INTCON, PEIE
+    BCF	PIR1, ADIF
+    BCF	PIR1, TMR2IF
+   
+    
+    
+    ;PARA PWM
+   
+    MOVLW .187
+    MOVWF    PR2
+    
+    BANKSEL  CCP1CON
+    BCF	CCP1CON, 7
+    BCF	CCP1CON, 6
+
+    BSF	CCP1CON, 3
+    BSF	CCP1CON, 2
+    BCF	CCP1CON, 1
+    BCF	CCP1CON, 0
+
+        
+    BANKSEL  CCP1CON 
+    BSF	CCP2CON, 3
+    BSF	CCP2CON, 2
+    BCF	CCP2CON, 1  
+    BCF	CCP2CON, 0
+   
+    BCF	T2CON, 6
+    BCF	T2CON, 5
+    BCF	T2CON, 4
+    BCF	T2CON, 3
+   
+   
+    BSF	T2CON, 1
+    BSF	T2CON, 0
+    BSF	T2CON, TMR2ON
+    
+    RETURN
+    
+    
+    
     END
