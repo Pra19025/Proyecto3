4074362dd141d72b48876176ef443c024bbf6d9e
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2020-11-25 16:51:06 -0600

    regresando a cuando solo se tenian dos servos

--- Proyecto3.X/Principal.asm
+++ Proyecto3.X/Principal.asm
@@ -1,3 +1,13 @@
+ ;*******************************************************************************
+;
+;   Filename:	    Laboratorio8 -> lab8.asm
+;   Date:		    8/10/2020
+;   File Version:	    v.1
+;   Author:		    Noel Prado
+;   Company:	    UVG
+;   Description:	    Manejo del módulo PWM
+;
+;*******************************************************************************  
 
     #include "p16f887.inc"
 
@@ -11,90 +21,50 @@
     GPR_VAR	    UDATA
     STATUS_TEMP	RES 1
     W_TEMP	RES 1
-    CONT1		RES 1
-    CONTROL_PWM RES 1
-    ADC0 RES 1
-    ADC1 RES 1
-    ADC2 RES 1
-    CONTADOR_ADC RES 1 ;CONTADOR QUE INDICA QUE CANAL SE ESTA LEYENDO
-    FLAGS RES 1
-    CONTADOR_PWM RES 1
-    AJUSTE_PWM RES 1 
-    TRABAJO1 RES 1 
-    TRABAJO2 RES 1 
-		
-    RES_VECT  CODE    0x0000            ; processor reset vector
-    GOTO    START                   ; go to beginning of program
 
+	    
+RES_VECT  CODE    0x0000            ; processor reset vector
+    GOTO    START                   ; go to beginning of program
 
 ISR_VECT    CODE 0X0004
  
+    
 PUSH:
     MOVWF	    W_TEMP
     SWAPF	   STATUS, W
     MOVWF	   STATUS_TEMP 
-
-    BTFSS   INTCON,T0IF
-    GOTO    INT_ADC
-    
-    BCF	INTCON,T0IF
-    MOVF    CONTROL_PWM,W
-    ADDWF   PCL, F
-    GOTO    INICIO
-    GOTO    PWM1
-    CLRF     CONTROL_PWM
-    
-    INICIO:
-    BSF	PORTC, RC0 
-    MOVF    TRABAJO1,W 
-    CLRF	TMR0
-    SUBWF   TMR0,F  ;TMRO =  256 - TRABAJO1
-    INCF     CONTROL_PWM,F ;A LA SIGUIENTE ENTRADA SE EJECUTA PWM1
-    GOTO INT_ADC
-    
-PWM1:
-    BCF	PORTC,RC0 ;AL PASAR EL TIEMPO ESTABLECIDO POR LA VARIABLE TRABAJO 1
-    MOVF   TRABAJO2,W
-    CLRF TMR0
-    SUBWF   TMR0,F ;256 - TRABAJO2
-    INCF  CONTROL_PWM,F
-    GOTO INT_ADC
-;***********************************************************LECTURA DE LOS 3 CANALES DEL ADC *******************************   
+        
 INT_ADC:
-    BTFSS   PIR1, ADIF 
-    GOTO    POP
-    BANKSEL ADRESH
     BCF	PIR1, ADIF
-    MOVF    CONTADOR_ADC,W
-    ADDWF   PCL,F   
-    GOTO    ES_ADC0
-    GOTO    ES_ADC1
-    GOTO    ES_ADC2
-    CLRF    CONTADOR_ADC
+    BANKSEL ADRESH	     
+    MOVF ADRESH,W		;GUARDAR 8 BITS EN RESULTHI
+    BTFSC   ADCON0, 2
+    GOTO	 CAMBIO_CANAL
+    MOVWF CCPR1L
+
+    BANKSEL ADCON0
+    BSF   ADCON0, 2
+    GOTO REINICIOADC
     
-    ES_ADC0:
-    MOVF    ADRESH, W
-    MOVWF   ADC0
-    BCF	ADCON0,CHS1
-    BSF	ADCON0,CHS0 ;SE SELECCIONA EL CANAL 1
-    GOTO FIN
+CAMBIO_CANAL:
+    ;SI YA CAMBIO EL CANAL EJECUTA ESTO
+    MOVWF   CCPR2L
+    BANKSEL ADCON0  
+    BCF   ADCON0, 2
+    GOTO    REINICIOADC
+
+INT_TMR2:
+    BCF	PIR1, TMR2IF
+    GOTO POP
     
-    ES_ADC1:
-    MOVF    ADRESH, W
-    MOVWF   ADC1
-    BSF	ADCON0,CHS1
-    BCF	ADCON0,CHS0 ;SE SELECCIONA EL CANAL 2
-    GOTO FIN
-    
-    ES_ADC2:
-    MOVF    ADRESH,W
-    MOVWF   ADC2
-    BCF	ADCON0,CHS1
-    BCF	ADCON0,CHS0 ;SE SELECCIONA CANAL 0
-    
-    FIN:
-    INCF    CONTADOR_ADC,F
-    BSF	ADCON0,GO ;INICIA CONVERSION
+REINICIOADC:
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    BANKSEL ADCON0
+    BSF ADCON0, GO
     
     
 POP:
@@ -105,64 +75,48 @@ POP:
     BSF	    INTCON, GIE
  
     RETFIE
-
-
+    
+    
+    
 
 MAIN_PROG CODE                      ; let linker place main program
+ 
 
 START
-
-
-    CALL    CONFIG_IO
-
-     
+ 
+    CALL	CONFIG_IO
+   
 LOOP:
+ 
     
-    MOVFW   ADC0
-    MOVWF   CCPR1L
-    
-    MOVFW    ADC1
-    MOVWF   CCPR2L
-    
-    MOVFW   ADC2
-    MOVWF TRABAJO1
-    	
+ 
 
     GOTO LOOP
     GOTO $                          ; loop forever
-
-
+    
+    
     
     
     CONFIG_IO
+
     
     BANKSEL PORTA
     CLRF    PORTC
     CLRF    PORTA
     CLRF    PORTB
-    CLRF    CONTROL_PWM
-    CLRF    ADC0
-    CLRF    ADC1
-    CLRF    ADC2
-    CLRF    CONTADOR_ADC
-    CLRF    FLAGS
-  
+    
     BANKSEL TRISA
     CLRF    TRISB
     CLRF    TRISC
     BSF	TRISA, 0
     BSF	TRISA, 1
-    BSF	TRISA, 2
-    
-    ;PARTE DEL ADC
     BANKSEL ADCON1
-    CLRF   ADCON1
+    MOVLW B'00000000'
+    MOVWF   ADCON1
     
     BANKSEL ANSEL
     BSF	ANSEL, 0
     BSF	ANSEL, 1
-    BSF	ANSEL, 2
-    CLRF    ANSELH
     BANKSEL ADCON0
     MOVLW   B'01000001'	
     MOVWF   ADCON0
@@ -172,8 +126,12 @@ LOOP:
     BSF	PIE1, ADIE
     BSF	PIE1, TMR2IE
     BANKSEL PORTA
-   BCF	PIR1, ADIF
-   BCF	PIR1, TMR2IF
+    BSF	INTCON, GIE
+    BSF	INTCON, PEIE
+    BCF	PIR1, ADIF
+    BCF	PIR1, TMR2IF
+   
+    
     
     ;PARA PWM
    
@@ -199,30 +157,16 @@ LOOP:
     BCF	T2CON, 6
     BCF	T2CON, 5
     BCF	T2CON, 4
-    BCF	T2CON, 3
-    BSF	T2CON, 2
-    BSF	T2CON, 1    ;BIT DE ENCENDIDO
-    BCF	T2CON, 0
+    BCF	T2CON, 3    
+   
+   
+    BSF	T2CON, 1
+    BSF	T2CON, 0
+    BSF	T2CON, TMR2ON
     
-    ;para timer 0, que se utiliza para el tercer servo
 
-    BANKSEL TRISA
-    BCF	    OPTION_REG, T0CS;	RELOJ INTERNO
-    BCF	    OPTION_REG, PSA;	PRESCALER A TMR0
-    BCF	    OPTION_REG, PS2;	SE PONE 111 PARA PRESCALER DE 256
-    BSF	    OPTION_REG, PS1
-    BSF	    OPTION_REG, PS0
-    BANKSEL PORTA
-    CLRF	    TMR0
-    BSF	    INTCON, T0IE
-    BCF	    INTCON, T0IF
+   
+   
     RETURN
-    
-    BANKSEL PORTA
-    BSF	INTCON, GIE
-    BSF	INTCON, PEIE
-
-    GOTO $                          ; loop forever
-
 
     END
